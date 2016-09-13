dfae869d8521f26f14662e3d137fa78e52addc8f
Portos123 <portos123@coursera-something.com>
2016-09-13 20:25:34 +0200

    Fixed and improvedcounting function

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -12,14 +12,15 @@
     $scope.message = "";
 
     $scope.checkIfTooMuch = function() {
-      $scope.lunch = String.prototype.trim($scope.lunch);
+      $scope.lunch = $scope.lunch.trim();
       if ($scope.lunch == "") {
         $scope.message = "Please enter data first";
         $scope.lunch_arr = [];
         $scope.styleName = "bad";
       } else {
         $scope.lunch_arr = $scope.lunch.split(",");
-        // todo check for empty elements
+        // remove empty elements
+        $scope.lunch_arr = $scope.lunch_arr.filter(function(e) { return e === 0 || e.trim() });
         if ($scope.lunch_arr.length <= 3) {
           $scope.message = "Enjoy!";
           $scope.styleName = "good";
