664324c4f3687e20d147880224b218032551a8ce
Portos123 <portos123@coursera-something.com>
2016-09-23 18:23:41 +0200

    module2 solution

---
+++ module2-solution/app.js
@@ -0,0 +1,41 @@
+(function(){
+  'use strict';
+
+  /* added polyfill when string trim function is not defined */
+  if (!String.prototype.trim) {
+    String.prototype.trim = function () {
+      return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+    };
+  }
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope) {
+    $scope.lunch = "";
+    $scope.lunch_arr = []; /* for further use */
+    $scope.styleName = "";
+    $scope.message = "";
+
+    $scope.checkIfTooMuch = function() {
+      $scope.lunch = $scope.lunch.trim();
+      if ($scope.lunch == "") {
+        $scope.message = "Please enter data first";
+        $scope.lunch_arr = [];
+        $scope.styleName = "bad";
+      } else {
+        $scope.lunch_arr = $scope.lunch.split(",");
+        // remove empty elements
+        $scope.lunch_arr = $scope.lunch_arr.filter(function(e) { return e === 0 || e.trim() });
+        if ($scope.lunch_arr.length <= 3) {
+          $scope.message = "Enjoy!";
+          $scope.styleName = "good";
+        } else {
+          $scope.message = "Too much!";
+          $scope.styleName = "good";
+        }
+      }
+    };
+  }
+})();
